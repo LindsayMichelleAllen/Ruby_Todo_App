b35a6312efa69eb369c517ae7fd458066b9d871b
Ubuntu <nanos.game.dev@gmail.com>
2023-07-23 23:04:06 +0000

    squash

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,4 @@ Rails.application.routes.draw do
   delete 'logout', to: 'session#destroy'
   get 'password', to: 'password#edit', as: 'edit_password'
   patch 'password', to: 'password#update'
-  get 'password/reset', to: 'password_reset#new'
-  post 'password/reset', to: 'password_reset#create'
-  get 'password/reset/edit', to: 'password_reset#edit'
-  patch 'password/reset/edit', to: 'password_reset#update'
 end
