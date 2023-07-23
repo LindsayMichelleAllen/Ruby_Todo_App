41fc0d9ccd2dd70135c4206fc4031338ffad4835
Ubuntu <nanos.game.dev@gmail.com>
2023-07-23 22:14:21 +0000

    update routes for user login

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "todo_tasks#index"
+  root "landing_page#index"
+  get 'home', to: 'landing_page#index'
+  get 'sign_up', to: 'registration#new'
+  post 'sign_up', to: 'registration#create'
+  get 'sign_in', to: 'session#new'
+  post 'sign_in', to: 'session#create', as: 'log_in'
+  delete 'logout', to: 'session#destroy'
+  get 'password', to: 'password#edit', as: 'edit_password'
+  patch 'password', to: 'password#update'
+  get 'password/reset', to: 'password_reset#new'
+  post 'password/reset', to: 'password_reset#create'
+  get 'password/reset/edit', to: 'password_reset#edit'
+  patch 'password/reset/edit', to: 'password_reset#update'
 end
