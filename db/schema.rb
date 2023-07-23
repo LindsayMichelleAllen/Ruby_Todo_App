bf77970b9163080720a6f261df0f4831ce132925
Ubuntu <nanos.game.dev@gmail.com>
2023-07-23 22:11:51 +0000

    squash with user create commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_22_203048) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_23_211614) do
   create_table "todo_tasks", force: :cascade do |t|
     t.string "description"
     t.datetime "reminder"
     t.datetime "dueby"
     t.boolean "completed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest"
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
