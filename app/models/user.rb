d939e8b014a7f181f269a0ecac6ac3d8ee7287db
Lindsay Michelle Allen <50183247+LindsayMichelleAllen@users.noreply.github.com>
2023-07-24 10:56:56 -0700

    comment user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+    #provided by bcrypt gem
     has_secure_password
-    
+    #create getter & setter
     attr_accessor :user
     
     validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
